56a58c6b91e0954ad37cd21f5caebb414041993e
NhanLe2426 <lethanhnhan0401@gmail.com>
2025-09-25 22:57:35 +0700

    Update Debug and Build for Exercise5

--- STM32_MCU_Lab2/Debug/Core/Src/exercise5.d
+++ STM32_MCU_Lab2/Debug/Core/Src/exercise5.d
@@ -1,5 +1,5 @@
-Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
- ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h \
+Core/Src/exercise5.o: ../Core/Src/exercise5.c ../Core/Inc/exercise5.h \
+ ../Core/Inc/main.h ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h \
  ../Core/Inc/stm32f1xx_hal_conf.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h \
@@ -23,8 +23,8 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim_ex.h \
- ../Core/Inc/software_timer.h ../Core/Inc/main.h ../Core/Inc/led_7seg.h \
- ../Core/Inc/exercise5.h ../Core/Inc/led_7seg.h
+ ../Core/Inc/led_7seg.h ../Core/Inc/led_7seg.h
+../Core/Inc/exercise5.h:
 ../Core/Inc/main.h:
 ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h:
 ../Core/Inc/stm32f1xx_hal_conf.h:
@@ -50,8 +50,5 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
 ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h:
 ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim.h:
 ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim_ex.h:
-../Core/Inc/software_timer.h:
-../Core/Inc/main.h:
 ../Core/Inc/led_7seg.h:
-../Core/Inc/exercise5.h:
 ../Core/Inc/led_7seg.h:
