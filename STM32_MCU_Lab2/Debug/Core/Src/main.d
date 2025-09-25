3f4540e0a6a9f7cac27681dae494ae5006bfa612
NhanLe2426 <lethanhnhan0401@gmail.com>
2025-09-25 16:37:38 +0700

    Update Debug and Build for Exercise4

--- STM32_MCU_Lab2/Debug/Core/Src/main.d
+++ STM32_MCU_Lab2/Debug/Core/Src/main.d
@@ -24,7 +24,7 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim_ex.h \
  ../Core/Inc/software_timer.h ../Core/Inc/main.h ../Core/Inc/led_7seg.h \
- ../Core/Inc/exercise3.h ../Core/Inc/led_7seg.h
+ ../Core/Inc/exercise4.h ../Core/Inc/led_7seg.h
 ../Core/Inc/main.h:
 ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h:
 ../Core/Inc/stm32f1xx_hal_conf.h:
@@ -53,5 +53,5 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
 ../Core/Inc/software_timer.h:
 ../Core/Inc/main.h:
 ../Core/Inc/led_7seg.h:
-../Core/Inc/exercise3.h:
+../Core/Inc/exercise4.h:
 ../Core/Inc/led_7seg.h:
